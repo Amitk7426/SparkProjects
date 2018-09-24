f9e65f314b15b885fe65f1102a5fa393a9440b18
Amit <bisen.amit01@gmail.com>
2018-09-24 23:42:33 +0530

    SparkExample2 with sortkey operations

---
+++ SparkExample2/src/main/scala/com/myCompany/scalaExample/SortKeyOperation.scala
@@ -0,0 +1,27 @@
+package com.myCompany.scalaExample
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SortKeyOperation {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("KeyOperations").setMaster("local[1]")
+    val sc = new SparkContext(conf)
+
+    val inputRDD = sc.textFile("C:\\Users\\Amit\\Desktop\\TestSpark\\SparkExample2\\input\\uber")
+    val header = inputRDD.first()
+    val format = new java.text.SimpleDateFormat("MM/dd/yyyy")
+    val days =Array("Sun","Mon","Tue","Wed","Thu","Fri","Sat")
+    val eliminate = inputRDD.filter(line => line != header)
+    val split = eliminate.map(line => line.split(",")).map { x => (x(0),format.parse(x(1)),x(3)) }
+    val combines = split.map(x => (x._1+" "+days(x._2.getDay),x._3.toInt))
+    combines.reduceByKey(_+_).map(item => item.swap).sortByKey(false).collect.foreach(println)
+
+    println("""++++++++++++++++++++++++++++++++++++++++++++++++++++++""")
+    combines.reduceByKey(_+_).map(item => item.swap).sortByKey(true).collect.foreach(println)
+
+    //for practice
+    val combine = split.map(x => (x._1+" "+x._2.getDay,x._3.toInt))
+    combine.collect
+  }
+}
