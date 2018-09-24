3c21034546e73d3d3764b64e46b960d39144355e
Amit <bisen.amit01@gmail.com>
2018-09-24 20:21:37 +0530

    SparkExample2 with JSON to DF

---
+++ SparkExample2/src/main/scala/com/myCompany/scalaExample/ReadJsonToDF.scala
@@ -0,0 +1,27 @@
+package com.myCompany.scalaExample
+
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql._
+
+object ReadJsonToDF {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("MaxPrice").setMaster("local[1]")
+    val sc = new SparkContext(conf)
+
+    val sqlContext = new SQLContext(sc)
+
+    val df = sqlContext.read.json("C:\\Users\\Amit\\Desktop\\TestSpark\\SparkExample2\\input\\multilinecolors.json")
+
+    df.printSchema()
+
+    df.registerTempTable("JSONdata")
+
+    val data=sqlContext.sql("select * from JSONdata")
+
+    data.show()
+
+    sc.stop
+  }
+}
